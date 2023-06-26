946f117d62270863ec4129c6cb0fa1af45cad3ca
EmilOrv <emiloo@stud.ntnu.no>
2023-06-26 13:05:42 +0200

    Added kontaktskjema kalkulator

--- Kalk - Vue/src/utils/loginutils.js
+++
@@ -1,6 +0,0 @@
-//Oversatt til services/service.js
-export function isLoginSuccessful(response){
-    if(response == undefined || response ==null) return false;
-    if(response.data == undefined || response.data==null) return false;
-    return response.data.status =="true";
-}
